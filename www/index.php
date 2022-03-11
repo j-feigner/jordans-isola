0945953a17dcfc83bba9660d6ed3230a861a2a99
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-03-11 10:00:51 -0800

    Initial php skeleton

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <p>Hello World</p>
+    <a href="piano/piano.php">Piano</a>
+</body>
+</html>
