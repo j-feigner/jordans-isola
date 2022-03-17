8c2f2c10b0b8b98dc2615444878790254197cbc7
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-03-17 15:05:45 -0700

    Initial commit of Indonesia Mergers Page

---
+++ www/indonesia_mergers/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="site-wrapper">
+        <div class="goods-selection">
+            <span class="good" id="rice">
+                <p>RICE</p>
+            </span>
+            <span class="good" id="spice">
+                <p>SPICE</p>
+            </span>
+            <span class="good" id="rubber">
+                <p>RUBBER</p>
+            </span>
+            <span class="good" id="siapfaji">
+                <p>SIAP FAJI</p>
+            </span>
+            <span class="good" id="oil">
+                <p>OIL</p>
+            </span>
+        </div>
+        <div class="asset-selection">
+            <div class="asset" id="asset1">
+                <input type="number" value="1" min="1">
+            </div>
+            <div class="asset" id="asset2">
+                <input type="number" value="1" min="1">
+            </div>
+        </div>
+    </div>
+    <script src="js/rectangle.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
