0f528b21e9f31a28731fe2af07bf9c16d05182f1
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-03-17 19:56:58 -0700

    Refactor into App object

--- www/indonesia_mergers/js/script.js
+++ www/indonesia_mergers/js/script.js
@@ -1,32 +1,47 @@
 window.onload = main;
 
 function main() {
-    var selected_good = null;
-    var good_value = 0;
+    var app = new App();
+    app.start();
+}
 
-    // Create good objects for each good type
-    var goods_objects = [];
-    var goods = document.querySelector(".goods-selection").children;
-    for(var i = 0; i < goods.length; i++) {
-        var good = {
-            element: goods[i],
-            overlay: goods[i].querySelector(".overlay"),
-            value: i === 0 ? 10 : 20 + (i * 5)
-        };
-        goods_objects.push(good);
+class App {
+    constructor() {
+        this.selected_good = null;
+        this.good_value = 0;
+        this.goods = null;
     }
 
-    // Add event listeners to each good object for changing
-    // user selection and updating value / overlays
-    goods_objects.forEach(good => {
-        good.element.addEventListener("click", () => {
-            if(selected_good !== null) { // Deselect previous good
-                selected_good.overlay.style.opacity = "0";
-            }
+    start() {
+        this.goods = this.createGoods();
+        this.setGoodsEvents();
+    }
 
-            selected_good = good;
-            good.overlay.style.opacity = "0.6";
-            good_value = good.value;
+    createGoods() {
+        var goods = [];
+        var selection = document.querySelector(".goods-selection").children;
+        for(var i = 0; i < selection.length; i++) {
+            var good = {
+                element: selection[i],
+                overlay: selection[i].querySelector(".overlay"),
+                value: i === 0 ? 10 : 20 + (i * 5)
+            };
+            goods.push(good);
+        }
+        return goods;
+    }
+
+    setGoodsEvents() {
+        this.goods.forEach(good => {
+            good.element.addEventListener("click", () => {
+                if(this.selected_good !== null) { // Deselect previous good
+                    this.selected_good.overlay.style.opacity = "0";
+                }
+    
+                this.selected_good = good;
+                this.good_value = good.value;
+                good.overlay.style.opacity = "0.6";
+            })
         })
-    })
+    }
 }
