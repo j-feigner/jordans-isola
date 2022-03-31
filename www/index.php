4c7460226e2ff84d7db8af4e80c88bfc4d8355c4
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-03-31 06:31:28 -0700

    Initial Styling

--- www/index.php
+++ www/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <p>Hello World</p>
-    <a href="piano/piano.php">Piano</a>
-</body>
-</html>
+<?php
+    $page = file_get_contents("home/html/index.html");
+    echo($page);
+?>
