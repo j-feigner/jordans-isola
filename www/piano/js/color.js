79b639aab16fa93c5131daf4f7602f3fb7e67e6c
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-04-03 16:31:41 -0700

    Added color fading method
    getFadeAtStep() returns a single color value given a ratio between two Color objects. This can be used to simulate a fade effect on the Canvas by changing the ratio value on given time interval.

--- www/piano/js/color.js
+++ www/piano/js/color.js
@@ -8,17 +8,21 @@ class Color {
 
     // Linearly interpolates between two Color objects and returns an array
     // of discrete Colors between start_color and end_color
-    // Expects color parameters to be string in form "rgb(r, g, b)"
     static createColorGradient(start_color, end_color, steps) {
         
     }
 
-    // Returns a Color between start_color and end_color given steps and time
-    static fade(start_color, end_color, steps) {
+    // Returns a Color between start_color and end_color at given ratio between the two
+    static getFadeAtStep(start_color, end_color, step) {
+        var dr = (start_color.r - end_color.r) * step;
+        var dg = (start_color.g - end_color.g) * step;
+        var db = (start_color.b - end_color.b) * step;
+        var da = (start_color.a - end_color.a) * step;
 
+        return new Color(dr, dg, db, da);
     }
 
-    // Returns string of form "rgb(r, g, b)" for CSS styling
+    // Returns string of form "rgb(r, g, b)" for CSS styling / HTMLCanvas manipulation
     // If include_alpha is true will return string of form "rgba(r, g, b, a)"
     toString(include_alpha) {
         if(include_alpha) {
