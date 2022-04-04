901d7a23143529745a99a9fbb2aafcb3918b8916
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-04-03 18:38:06 -0700

    Piano key fade effect (issue #4)

--- www/piano/js/color.js
+++ www/piano/js/color.js
@@ -3,7 +3,10 @@ class Color {
         this.r = red;
         this.g = green;
         this.b = blue;
+
         this.a = alpha;
+
+        this.fade_interval;
     }
 
     // Linearly interpolates between two Color objects and returns an array
@@ -20,6 +23,30 @@ class Color {
         var da = (start_color.a - end_color.a) * step;
 
         return new Color(dr, dg, db, da);
+    }
+
+    // Fades from current color to end_color over time milliseconds
+    fade(end_color, time) {
+        var step = time / 50;
+
+        var dr = (this.r - end_color.r) / 50;
+        var dg = (this.g - end_color.g) / 50;
+        var db = (this.b - end_color.b) / 50;
+        var da = (this.a - end_color.a) / 50;
+
+        var timer = 0;
+
+        this.fade_interval = setInterval(() => {
+            this.r -= dr;
+            this.g -= dg;
+            this.b -= db;
+            this.a -= da;
+
+            timer += step;
+            if(timer >= time) {
+                clearInterval(this.fade_interval);
+            }
+        }, step);
     }
 
     // Returns string of form "rgb(r, g, b)" for CSS styling / HTMLCanvas manipulation
