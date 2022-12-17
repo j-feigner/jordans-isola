0c1318718f1c3b0112febff2e5855eb42335415f
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-12-16 16:25:56 -0800

    Site Footer dynamic loading

--- www/index.php
+++ www/index.php
@@ -1,3 +1,169 @@
-<?php
-    include("home/html/index.html");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jordan's Isola</title>
+    <link rel="stylesheet" href="home/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400&family=Roboto:wght@300;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div id="site-wrapper">
+        <header>
+            <div id="site-header">
+                <h1>Some stuff that Jordan has made.</h1>
+                <!-- <div class="heading-underline"></div> -->
+             </div>
+             <div id="social-links">
+                <div class="link-arrow-button" id="github-top-link">
+                    <a href="https://github.com/j-feigner" target="_blank"><button>
+                        <div class="web-icon-wrapper">
+                            <img src="home/images/GitHub-Mark-Light-64px.png">
+                        </div>
+                        <div class="button-body-wrapper">
+                            <span>GitHub</span>
+                            <img src="home/images/dropdown_arrow.svg" class="moving-arrow">
+                        </div>
+                    </button></a>
+                </div>
+                <div class="link-arrow-button" id="linkedin-top-link">
+                    <a href="https://linkedin.com/in/jordan-feigner" target="_blank"><button>
+                        <div class="web-icon-wrapper">
+                            <img src="home/images/LI-In-Bug.png">
+                        </div>
+                        <div class="button-body-wrapper">
+                            <span>LinkedIn</span>
+                            <img src="home/images/dropdown_arrow.svg" class="moving-arrow">
+                        </div>
+                    </button></a>
+                </div>
+             </div>
+        </header>
+        <main>
+            <div id="main-content-wrapper">
+                <div id="main-content">
+                    <div id="larger-projects" class="project-group">
+                        <div class="page-link big">
+                            <div class="link-image">
+                                <img src="home/images/bgg_ratings_link.png">
+                                <a href="pages/bggratingstool">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h4>BGG Rating Tool</h4>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                                <p>Compare and correlate user ratings in the BoardGameGeek database</p>
+                                <ul>
+                                    <li>Interfaces with the BGG XML API for pulling user collection data.</li>
+                                    <li>Analyzes data with both the Pearson and Spearman Ranked correlation coefficients to determine similarity in users' tastes in games.</li>
+                                    <li>Provides lists of games two users mutually like/dislike or agree/disagree about based off of their normalized scores.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="page-link big">
+                            <div class="link-image">
+                                <img src="home/images/music_sequencer_link.png">
+                                <a href="pages/musicsequencer">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h4>Music Sequencer</h4>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                    </div>
+                    <div id="misc-projects" class="project-group">
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/sierpinski_link.png">
+                                <a href="pages/sierpinski">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Sierpinski Triangle Generator</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/piano_link_img.png">
+                                <a href="pages/piano">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Virtual Piano</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/guitar_link.png">
+                                <a href="pages/guitar">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Virtual Guitar</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/indonesia_link.png">
+                                <a href="pages/indonesiamergers">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Mergers Calculator<br>(Indonesia)</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/fresh_fish_link.png">
+                                <a href="pages/freshfish">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Expropriation Helper<br>(Fresh Fish)</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                        <div class="page-link small">
+                            <div class="link-image">
+                                <img src="home/images/wotr_link.png">
+                                <a href="pages/wotrbattletool">
+                                    <div class="image-overlay"></div>
+                                </a>
+                                <div class="link-label">
+                                    <h5>Battle Odds<br>(War of the Ring)</h5>
+                                </div>
+                            </div>
+                            <div class="link-description">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <?php 
+            include("home/html/site_footer.html");
+        ?>
+    </div>
+</body>
+</html>
